e53df9491ad14bdb345332d5615f4bdcf9246a7a
joaovitorsantanasdsjohn-crypto <joaovitorsantanasdsjohn@gmail.com>
2025-11-03 09:50:20 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Configurações do Telegram e limites de envio
+TELEGRAM_TOKEN = "SEU_TOKEN_AQUI"
+TELEGRAM_CHAT_ID = "SEU_CHAT_ID_AQUI"
+
+# Limites de envio
+MAX_MESSAGES_PER_HOUR = 5
+
+# Fontes de notícias
+NEWS_SOURCES = [
+    "https://www.investing.com/news/forex-news",
+    "https://www.investing.com/news/cryptocurrency-news",
+    "https://www.investing.com/news/commodities-news",
+    "https://www.investing.com/news/stock-market-news"
+]
